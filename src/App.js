3f2f51cd0de641f62f796ee3edf94c94a447cf6f
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-01 18:12:17 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,50 +1,82 @@
-import React, { useRef, useState } from "react";
-import PostForm from "./component/PostForm";
-import PostList from "./component/PostList";
-import MyButton from "./component/UI/button/MyBtn";
-import MyInput from "./component/UI/input/MyInput";
+import React, { useRef, useState } from 'react';
+import PostForm from './component/PostForm';
+import PostList from './component/PostList';
+import MyInput from './component/UI/input/MyInput';
+import MySelect from './component/UI/select/MySelect';
+import Select from './component/UI/select/MySelect';
 
-import "./styles/App.css";
+import './styles/App.css';
 
 function App() {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      title: "JavaScript",
-      body: "JavaScript - великолепный язык програмированния",
+      title: 'JavaScript',
+      body: 'JavaScript - великолепный язык програмированния',
     },
     {
       id: 2,
-      title: "React",
-      body: "React - великолепный язык програмированния",
+      title: 'React',
+      body: 'React - великолепный язык програмированния',
     },
     {
       id: 3,
-      title: "Node",
-      body: "Node - великолепный язык програмированния",
+      title: 'Node',
+      body: 'Node - великолепный язык програмированния',
     },
     {
       id: 4,
-      title: "React.native",
-      body: "React.native - великолепный язык програмированния",
+      title: 'React.native',
+      body: 'React.native - великолепный язык програмированния',
     },
   ]);
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuerry, setSearchQuerry] = useState('');
 
-  const createPost = (newPost) => {
+  const createPost = newPost => {
     setPosts([...posts, newPost]);
   };
 
-  const removePost = (post) => {
-    setPosts(posts.filter((item) => item.id !== post.id));
+  function getSortedPosts() {
+    console.log('Done');
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts();
+
+  const removePost = post => {
+    setPosts(posts.filter(item => item.id !== post.id));
+  };
+
+  const sortPost = sort => {
+    setSelectedSort(sort);
   };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0px' }} />
+      <MyInput
+        value={searchQuerry}
+        onChange={e => setSearchQuerry(e.target.value)}
+        placeholder="Поиск..."
+      />
+      <MySelect
+        defaultValue=" Сортировка по"
+        options={[
+          { value: 'title', name: 'По названию' },
+          { value: 'body', name: 'По описанию' },
+        ]}
+        value={selectedSort}
+        onChange={sortPost}
+      />
       {posts.length ? (
-        <PostList remove={removePost} posts={posts} title={"Список постов"} />
+        <PostList remove={removePost} posts={sortedPosts} title={'Список постов'} />
       ) : (
-        <h2 style={{ textAlign: "center" }}>Посты не найдены!</h2>
+        <h2 style={{ textAlign: 'center' }}>Посты не найдены!</h2>
       )}
     </div>
   );
